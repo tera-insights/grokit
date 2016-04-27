6adb8770d917871febd338d702458a446f286849
Jonathan Claus <jonathanmclaus@gmail.com>
2016-04-27 17:45:11 -0400

    Added BlockScheduler

--- Libs/base/include/gist.h
+++ Libs/base/include/gist.h
@@ -20,8 +20,8 @@ class AnswerGLA {
     bool answer_;
 };
 
-// This LocalScheduler is templated with Task type T and returns a single Task
-// using the value it is initialized with.
+// This scheduler is templated with Task type T and returns a single Task using
+// the value it is initialized with.
 template<class T>
 class SimpleScheduler {
  private:
@@ -39,3 +39,25 @@ class SimpleScheduler {
     return finished = !finished;
   }
 };
+
+// This scheduler is templated Task type T and is initialized with an upper and
+// lower bound. It returns one task for each value in the interval [begin, end).
+// T should support a pre-increment operator and an equality comparison. It can
+// be used with iterators instead of simple integer indices, but the object that
+// is being iterated over must have a lifespan of at least a round for the GIST.
+template<class T>
+class BlockScheduler {
+ private:
+  // The current and last (exclusive) task to be outputted by this scheduler.
+  T current_, end_;
+
+ public:
+  BlockScheduler(T begin, T end) : current_(begin), end_(end) {};
+
+  bool GetNextTask(T& task) {
+    if (current_ == end_)
+      return false;
+    task = current_;
+    ++current;
+  }
+}
