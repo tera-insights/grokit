e9137bf47a68140cc6848ac9a1dbbce6bdf6bf9b
Jonathan Claus <jonathanmclaus@gmail.com>
2016-05-24 12:40:02 -0400

    Added tcmalloc option

--- src/PHP/php/grokit_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$__grokit_config_segment_bits =  26;
-$__grokit_config_num_segs =  64;
-$__grokit_config_exec_threads = 48;
-$__grokit_config_cpu_cleaners = 48;
-$__grokit_config_disk_tokens = 48;
-$__grokit_config_cleaner_disk_requests = 48;
-?>
