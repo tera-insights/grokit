90c8ee339234eccc20ddbfd2cfa4146f8dbcac89
Jonathan Claus <jonathanmclaus@gmail.com>
2016-06-20 10:52:06 -0400

    Small formatting changes

--- Libs/base/UDFs/Date_Functions.php
+++ Libs/base/UDFs/Date_Functions.php
@@ -1,24 +1,24 @@
 <?php
 
 declareFunction('DATE', ['BASE::DATETIME'], function($args) {
-	$dateType = lookupType('BASE::DATE');
-	$name = generate_name("DateTimeToDate");
+    $dateType = lookupType('BASE::DATE');
+    $name = generate_name("DateTimeToDate");
 ?>
 
 inline
 DATE <?=$name?>( const DATETIME& dt ) {
-	DATE d(dt.Year(), dt.Month(), dt.Day());
-	return d;
+  DATE d(dt.Year(), dt.Month(), dt.Day());
+  return d;
 }
 
 <?
-	return [
-		'kind'		=> 'FUNCTION',
-		'name'		=> $name,
-		'input'		=> $args,
-		'result'	=> $dateType,
-		'deterministic' => true,
-	];
+  return [
+      'kind'          => 'FUNCTION',
+      'name'          => $name,
+      'input'         => $args,
+      'result'        => $dateType,
+      'deterministic' => true,
+  ];
 });
 
 // Constructs a DateTime from a Date and a Time.
